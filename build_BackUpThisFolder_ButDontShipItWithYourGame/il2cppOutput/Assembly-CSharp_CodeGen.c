41455b0678406b2db06c274fafb9340b30c2be29
Liaw Xin Yan <46766154+Lemonsr@users.noreply.github.com>
2024-03-03 14:35:46 +0800

    Fix Scrollbar

---
+++ build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FurniturePlacementManager::Update()
+extern void FurniturePlacementManager_Update_mF3C776EBE5CBEA6BD6049FD1C5CA096303FAEE04 (void);
+// 0x00000002 System.Void FurniturePlacementManager::PlaceFurniture(UnityEngine.Vector3,UnityEngine.Quaternion)
+extern void FurniturePlacementManager_PlaceFurniture_mB79682D22CBF00E62380222B8B541779AEA8F79C (void);
+// 0x00000003 System.Void FurniturePlacementManager::MoveFurniture(UnityEngine.Vector2)
+extern void FurniturePlacementManager_MoveFurniture_m5CA59452D0E62C098A0AFC69183BA32046DAA7E9 (void);
+// 0x00000004 System.Void FurniturePlacementManager::DeleteSelectedFurniture()
+extern void FurniturePlacementManager_DeleteSelectedFurniture_m9AF876B234102E81D854705C0F00DDC89B4D89A8 (void);
+// 0x00000005 System.Boolean FurniturePlacementManager::isButtonPressed()
+extern void FurniturePlacementManager_isButtonPressed_mFEB29D73E7D6AE832D0AFD7562903FA0D3441226 (void);
+// 0x00000006 System.Void FurniturePlacementManager::SwitchFurniture(UnityEngine.GameObject)
+extern void FurniturePlacementManager_SwitchFurniture_mEAA6810B8FB8D6DDEAD74962B8CBB55A7EA8FC73 (void);
+// 0x00000007 System.Void FurniturePlacementManager::.ctor()
+extern void FurniturePlacementManager__ctor_mEA34A204C9BA9102AF4F07C7468DC544B975087E (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	FurniturePlacementManager_Update_mF3C776EBE5CBEA6BD6049FD1C5CA096303FAEE04,
+	FurniturePlacementManager_PlaceFurniture_mB79682D22CBF00E62380222B8B541779AEA8F79C,
+	FurniturePlacementManager_MoveFurniture_m5CA59452D0E62C098A0AFC69183BA32046DAA7E9,
+	FurniturePlacementManager_DeleteSelectedFurniture_m9AF876B234102E81D854705C0F00DDC89B4D89A8,
+	FurniturePlacementManager_isButtonPressed_mFEB29D73E7D6AE832D0AFD7562903FA0D3441226,
+	FurniturePlacementManager_SwitchFurniture_mEAA6810B8FB8D6DDEAD74962B8CBB55A7EA8FC73,
+	FurniturePlacementManager__ctor_mEA34A204C9BA9102AF4F07C7468DC544B975087E,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	4410,
+	1967,
+	3650,
+	4410,
+	4246,
+	3575,
+	4410,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
